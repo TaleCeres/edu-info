b85d55425210ac05131148c0616b25682fd0f3e6
Bozens <771845116@qq.com>
2019-12-27 15:35:32 +0800

    去除console.log

--- src/api/activity.js
+++ src/api/activity.js
@@ -2,9 +2,7 @@ import request from '@/utils/request'
 import store from '../store'
 
 export function getActivityList () {
-  console.log(store)
   const org = store.state.org
-  console.log('org', org)
   return request({
     url: '/activity/list',
     method: 'get',
